c24a7b3ae9ef78d803670536a518cdd37a35d1aa
Sourabh Mehta <somehta@adobe.com>
2020-06-20 04:16:22 +0530

    Fixing Error on click of submit on Edit Image page

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -148,7 +148,7 @@ public class ImageController {
         updatedImage.setDate(new Date());
 
         imageService.updateImage(updatedImage);
-        return "redirect:/images/" + updatedImage.getTitle();
+        return "redirect:/images/" +updatedImage.getId()+"/"+ updatedImage.getTitle();
     }
 
 
